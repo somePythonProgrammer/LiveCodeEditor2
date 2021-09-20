b23802b301ef9775fba1384d8b69fb6b94365054
E - Coders (Aasheesh Agarwal) <83082760+e-coders@users.noreply.github.com>
2021-09-20 01:08:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+openPage(event, 'OUTPUT');
 function showPreview(){
 	var htmlCode = document.getElementById("htmlCode").value;
 	var cssCode = "<style>" + document.getElementById("cssCode").value + "</style>";
